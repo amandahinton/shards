e3f3b0377d55336e756ca649144bdecdabac9c5a
Amanda <hello@amandahinton.com>
2023-08-23 22:02:54 -0400

    set up app

---
+++ App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>This text comes from App.js</Text>
+      <Text>Wow, good job!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    color: "#f20091",
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+});
